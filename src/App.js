ff3a42af820eb8d6a8edc31fd68def1085228927
Paul Levitsky <85572670+Pasha127@users.noreply.github.com>
2022-09-23 14:15:13 -0400

    music player update

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import { Router } from 'react-router-dom';
 import Home from './Components/Home';
 import MyNavbar from './Components/MyNavbar';
+import MusicPlayer from './Components/MusicPlayer';
 function App() {
   return (<>
     <MyNavbar/>
     <Router>
       <Home />
     </Router>
+    <MusicPlayer/>
   </>
   );
 }
