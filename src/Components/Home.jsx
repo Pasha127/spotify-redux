35819a9aaf7978710553c90c3367f51c2d7bf119
Pasha127 <pasha.account@gmail.com>
2022-09-23 14:29:59 +0200

    will delete this and start again

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,7 @@
 
 const Home = ()=>{
     return(
-        
+        <></>
     )
 }
 export default Home
---
+++ src/Components/MyNavbar.jsx
@@ -0,0 +1,9 @@
+
+
+const MyNavbar = ()=>{
+return(<>
+
+</>)
+    }
+
+    export default MyNavbar
